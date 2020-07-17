957261e1fd1182f3de926f7e5d8689ebcb0b6b01
BrandyBecker <brandybeckerfsw@gmail.com>
2020-07-17 11:55:29 -0400

    mvp for sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -28,9 +28,29 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+reversed_path = []
+rooms = {}
+opposite_Directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
+rooms[0] = player.current_room.get_exits()
 
+while len(rooms) < len(room_graph) - 1:
+    if player.current_room.id not in rooms:
+        rooms[player.current_room.id] = player.current_room.get_exits()
+        lastRoom = reversed_path[-1]
+        rooms[player.current_room.id].remove(lastRoom)
 
+    while len(rooms[player.current_room.id]) < 1:
+        reverse = reversed_path.pop()
+        traversal_path.append(reverse)
+        player.travel(reverse)
+
+    exit_dir = rooms[player.current_room.id].pop(0)
+    traversal_path.append(exit_dir)
+    reversed_path.append(opposite_Directions[exit_dir])
+    player.travel(exit_dir)
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
